c6537f40b1f084947fc62683fdfa3b8dff3b576d
Dor Gamliel <gamlieldor@gmail.com>
2018-04-02 16:59:36 +0300

    updating setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='py-package',
+    name='py-package-template',
     version='0.1.0',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='An example python package',
     long_description=open('README.rst').read(),
     install_requires=[],
-    url='https://github.com/gamlieldor/py-package',
+    url='https://github.com/gamlieldor/py-package-template',
     author='Dor Gamliel',
     author_email='gamlieldor@gmail.com'
 )
